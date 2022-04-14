bf00d85e567bf35e5b5e73a1ad1129d42b338fba
joshbaker0212 <103048416+joshbaker0212@users.noreply.github.com>
2022-04-14 11:04:45 -0400

    Update webmonitor.py

--- webmonitor.py
+++ webmonitor.py
@@ -1,10 +1,16 @@
 import time
 import hashlib
 from urllib.request import urlopen, Request
+import ssl
  
 # website to monitor
 url = Request('http://34.226.88.255/',
               headers={'User-Agent': 'Mozilla/5.0'})
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+content = urlopen(url, context=ctx).read()
  
 #read website and store it in webcontent
 webcontent = urlopen(url).read()
